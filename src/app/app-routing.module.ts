29a9f541a4a76cd3fffc253c7a1996a5e53da977
hesamnmt <hesamnmt2001@gmail.com>
2024-05-15 14:35:49 +0330

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,11 @@ const routes: Routes = [
       ),
   },
   {
+    path: 'post',
+    loadChildren: () =>
+      import('./modules/posts/posts.module').then((m) => m.PostsModule),
+  },
+  {
     path: '',
     redirectTo: '',
     pathMatch: 'full',
