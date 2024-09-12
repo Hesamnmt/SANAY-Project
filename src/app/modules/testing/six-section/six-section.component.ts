515286b9bdccc3be60115a7d34b38d656c1c6876
hesamnmt <hesamnmt2001@gmail.com>
2024-09-12 19:22:16 +0330

    add section 4

--- src/app/modules/testing/six-section/six-section.component.ts
+++ src/app/modules/testing/six-section/six-section.component.ts
@@ -4,10 +4,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import {
   debounceTime,
   fromEvent,
+  interval,
   map,
+  mergeMap,
   pipe,
   reduce,
   scan,
+  switchMap,
+  takeUntil,
   tap,
   throttleTime,
 } from 'rxjs';
@@ -27,28 +31,53 @@ export class SixSectionComponent {
     });
   }
 
+  //----------- find previenc and city ----------
+
+  ngAfterViewInit() {
+    // fromEvent(this.searchInput.nativeElement, 'input')
+    //   .pipe(
+    //     debounceTime(500),
+    //     map((event: any) => event.target.value),
+    //     tap((value: string) => console.log('Final input:', value))
+    //   )
+    //   .subscribe((res) => {
+    //     this.apiService.getGithubUser(res).subscribe();
+    //   });
+
+    //----------- find previenc and city with switchMap ----------
+
+    fromEvent(this.searchInput.nativeElement, 'input')
+      .pipe(
+        debounceTime(500),
+        map((event: any) => event.target.value),
+        tap((value: string) => console.log('Final input:', value)),
+        switchMap((val) => this.apiService.getGithubUser(val))
+      )
+      .subscribe();
+  }
+
+  //----------------------------------
+  // fromEvent(document, 'mousedown')
+  //   .pipe(
+  //     tap((val) => console.log('tap:', val)),
+  //     mergeMap(() =>
+  //       interval(10).pipe(takeUntil(fromEvent(document, 'mouseup')))
+  //     )
+  //   )
+  //   .subscribe(console.log);
+
+  // ----------------------------
+
   ngOnInit() {
     this.myForm
       .get('input')
       ?.valueChanges.pipe(debounceTime(500))
       .subscribe((res: any) => {
-        this.apiService
-          .getGithubUser(res)
-          .pipe(map((val: any) => val.login))
-          .subscribe(console.log);
+        return;
       });
-  }
 
-  //----------- find previenc and city ----------
-  ngAfterViewInit() {
-    fromEvent(this.searchInput.nativeElement, 'input')
-      .pipe(
-        debounceTime(500),
-        map((event: any) => event.target.value),
-        tap((value: string) => console.log('Final input:', value))
-      )
-      .subscribe((res) => {
-        this.apiService.getGithubUser(res).subscribe();
-      });
+    fromEvent(document, 'mousedown')
+      .pipe(switchMap(() => interval(1000)))
+      .subscribe(console.log);
   }
 }
